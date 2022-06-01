674ca4bbf65f294be487d79d0bcfbe44b54dde61
Cédric Michel <c-e-d@live.fr>
2022-06-01 17:38:16 +0200

    bills#2 extensions

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -20,11 +20,13 @@ export default class NewBill {
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
     const filePath = e.target.value.split(/\\/g)
     const fileName = filePath[filePath.length-1]
+    const fileExtension = fileName.split(".").pop().toLowerCase();
     const formData = new FormData()
     const email = JSON.parse(localStorage.getItem("user")).email
     formData.append('file', file)
     formData.append('email', email)
 
+    if (fileExtension.match(/^(jpeg|jpg|png)$/)) {
     this.store
       .bills()
       .create({
@@ -39,6 +41,11 @@ export default class NewBill {
         this.fileUrl = fileUrl
         this.fileName = fileName
       }).catch(error => console.error(error))
+    } else {
+      alert(
+        "Merci d'envoyer uniquement des fichiers au format jpg, jpeg ou png."
+      )
+    }
   }
   handleSubmit = e => {
     e.preventDefault()
