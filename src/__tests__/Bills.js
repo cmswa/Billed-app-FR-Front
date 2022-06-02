0719da2040fb4bedb66b9d2242fb39940bd034a2
Cédric Michel <c-e-d@live.fr>
2022-06-02 20:22:22 +0200

    Bills - tests unitaires

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,13 +2,15 @@
  * @jest-environment jsdom
  */
 
-import {screen, waitFor} from "@testing-library/dom"
+import { screen, waitFor, fireEvent } from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
-import {localStorageMock} from "../__mocks__/localStorage.js";
+import { ROUTES_PATH } from "../constants/routes.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import Bills from '../containers/Bills';
 
 import router from "../app/Router.js";
+import { ROUTES } from "../constants/routes.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -26,6 +28,7 @@ describe("Given I am connected as an employee", () => {
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
       //to-do write expect expression
+      expect(screen.getByTestId('icon-window').classList.contains('active-icon')).toBeTruthy()
 
     })
     test("Then bills should be ordered from earliest to latest", () => {
@@ -34,6 +37,42 @@ describe("Given I am connected as an employee", () => {
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
+    })
+  })
+
+  describe('When I am on Bills Page, i click on the new bill button', () => {
+    test('I should be sent to the new bill page', () => {
+
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({
+          pathname
+        })
+      }
+      document.body.innerHTML = BillsUI({
+        data: bills
+      })
+      const bill = new Bills({
+        document,
+        onNavigate,
+        store: null,
+        bills,
+        localStorage: window.localStorage
+      })
+      $.fn.modal = jest.fn()
+
+      const handleClickNewBill = jest.fn((e) => bill.handleClickNewBill(e))
+
+      const iconNewBill = screen.getByTestId('btn-new-bill')
+      iconNewBill.addEventListener('click', handleClickNewBill)
+      fireEvent.click(iconNewBill)
+      expect(handleClickNewBill).toHaveBeenCalled()
+
+      const modale = screen.getAllByTestId('form-new-bill')
+      expect(modale).toBeTruthy()
     })
   })
 })
